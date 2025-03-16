067d15d79c0af068fde9d42c9470feb3065cefa9
Glitch (topic-02-lab-rb-25) <none>
2025-03-16 23:17:02 +0000

    🎆🔦 Checkpoint ./views/about.hbs:73922512/333 ./controllers/about.js:73922512/191

--- controllers/about.js
+++ controllers/about.js
@@ -1,9 +1,13 @@
 'use strict';
 
+import logger from "../utils/logger.js";
+import appStore from "../models/app-store.js";
+
 const about = {
   createView(request, response) {
     const viewData = {
       title: "About page",
+      info: appStore.getAppInfo()
     };
     response.render('about', viewData);   
   },
