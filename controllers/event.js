73defff708f31ca638f25fdf8adcc06c7813a5fe
Glitch (topic-02-lab-rb-25) <none>
2025-05-04 00:25:42 +0000

    🌊🐥 Checkpoint ./views/event.hbs:73922512/663 ./views/partials/listevents.hbs:73922512/1491 ./views/partials/addevent.hbs:73922512/663 ./controllers/event.js:73922512/150

--- controllers/event.js
+++ controllers/event.js
@@ -20,9 +20,11 @@ const event = {
       const categoryId = request.params.id;
       const category = cubeStore.getEventCategory(categoryId);
       const newevent = {
-        id: uuidv4(),
-        title: request.body.title,
-        artist: request.body.artist,
+        event_id: uuidv4(),
+        name: request.body.name,
+        cutoff: request.body.cutoff,
+        avg_type: request.body.avg_type,
+        wr_vid: request.body.wr_vid,
       };
       cubeStore.addEvent(categoryId, newevent);
       response.redirect('/event/' + categoryId);
