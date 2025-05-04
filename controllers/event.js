fe3dbdb74e9c028574e4c265039aab87262c0d77
Glitch (topic-02-lab-rb-25) <none>
2025-05-04 01:56:02 +0000

    🌷🍿 Checkpoint ./views/partials/listcategories.hbs:73922512/2330 ./controllers/event.js:73922512/90 ./views/partials/addcategory.hbs:73922512/20 ./views/partials/addevent.hbs:73922512/21

--- controllers/event.js
+++ controllers/event.js
@@ -16,20 +16,20 @@ const event = {
     response.render('event', viewData);   
   },
   
-addEvent(request, response) {
-  const categoryId = request.params.id;
-  const category = cubeStore.getEventCategory(categoryId);
-  const newEvent = {
-    event_id: uuidv4(),
-    name: request.body.name,
-    cutoff: request.body.cutoff,
-    avg_type: request.body.avg_type,
-    wr_vid: request.body.wr_vid,
-  };
+  addEvent(request, response) {
+    const categoryId = request.params.id;
+    const category = cubeStore.getEventCategory(categoryId);
+    const newEvent = {
+      event_id: uuidv4(),
+      name: request.body.name,
+      cutoff: request.body.cutoff,
+      avg_type: request.body.avg_type,
+      wr_vid: request.body.wr_vid,
+    };
 
-  cubeStore.addEvent(categoryId, newEvent);
-  response.redirect(`/event/${categoryId}`);
-}
+    cubeStore.addEvent(categoryId, newEvent);
+    response.redirect(`/event/${categoryId}`);
+  }
 
 
 };
