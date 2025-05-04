2e30c937009cf49c99d0eb99315202d7488f8fd9
Glitch (topic-02-lab-rb-25) <none>
2025-05-04 23:24:13 +0000

    🔋☺️ Checkpoint ./controllers/accounts.js:73922512/741

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -49,9 +49,9 @@ const accounts = {
   //authenticate function to check user credentials and either render the login page again or the start page.
   authenticate(request, response) {
     const user = userStore.getUserByEmail(request.body.email);
-
     if (user && request.body.password === user.password) {
       response.cookie('category', user.email);
+      logger.info('logging in' + user.email);
       response.redirect('/start');
     } else {
       response.redirect('/login');
